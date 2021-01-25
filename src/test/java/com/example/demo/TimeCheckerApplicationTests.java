bc3a167ed81570ff427ab849a05d79820ef26dac
Rasika <info.rasika@gmail.com>
2021-01-26 05:24:19 +0530

    Add docs

--- src/test/java/com/example/demo/TimeCheckerApplicationTests.java
+++ src/test/java/com/example/demo/TimeCheckerApplicationTests.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2021, com.github.rasika (https://github.com/rasika) All Rights Reserved.
+ *
+ *  com.github.rasika licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
@@ -15,6 +32,11 @@ import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Tests for accessing basic functionality
+ *
+ * @since 1.0.0
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TimeCheckerApplicationTests {
     private static final String URL = "http://localhost:";
